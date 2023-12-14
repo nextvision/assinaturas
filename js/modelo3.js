4ea391e20de0f06256fe7a2e1c95020d82edbdf3
ipedroavelino <ipedroavelino@gmail.com>
2023-12-14 16:14:25 -0300

    Add display

--- js/modelo3.js
+++ js/modelo3.js
@@ -4,6 +4,7 @@ async function acionarInput(event) {
     // Verifica se a tecla pressionada é "Enter" (código 13)
     if (event.key === 'Enter') {
         await consultarDados();
+        mostrarElemento()
     }
 }
 
@@ -31,6 +32,8 @@ async function consultarDados(){
     }
 }
 
+ 
+
 function buscaRegistro(data, nome){
 
     return  data.find(item => item.nome.toLowerCase().includes(nome.toLowerCase()));
@@ -38,17 +41,25 @@ function buscaRegistro(data, nome){
 
 function exibirDados(registro){ 
     
+
+    
     document.getElementById('nome').innerText = registro.nome;
     document.getElementById('email').innerText = registro.email;
     document.getElementById('telefone').innerText = registro.telefone;
     document.getElementById('telefone2').innerText = registro.telefone2;
-    document.getElementById('cargo').innerText = registro.cargo
 
-    if(cargo.textContent.trim() === ''){
-        document.getElementById('cargo').style.display = 'none'
+    var cargoParagrafo = document.getElementById('cargo');
 
-    }else{
-        document.getElementById('cargo').innerText = registro.cargo
+    if (registro.cargo) {
+        // Se houver um valor de cargo, exibe o parágrafo
+        cargoParagrafo.style.display = 'block';
+        cargoParagrafo.innerText = registro.cargo;
+    } else {
+        // Se não houver valor de cargo, oculta o parágrafo
+        cargoParagrafo.style.display = 'none';
     }
 }
  
+function mostrarElemento() {
+    document.getElementById('elementoCondicional').style.display = 'block';
+}
